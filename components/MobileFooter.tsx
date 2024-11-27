b86fcec6c7e2234b5dec71d0c217cf0e1cc5def7
Munrhalls <antarcticdepths71@gmail.com>
2024-11-27 18:09:04 +0100

    SIGN IN @footer

--- components/MobileFooter.tsx
+++ components/MobileFooter.tsx
@@ -1,11 +1,23 @@
 "use client";
 
-import { FaBars, FaSearch, FaShoppingBag, FaUser } from "react-icons/fa";
+import { FaBars, FaSearch, FaShoppingBag, FaUser, FaKey } from "react-icons/fa";
 import { useToggleDrawer, useStore } from "../store";
-
+import { ClerkLoaded } from "@clerk/nextjs";
+import { SignInButton, UserButton, useUser } from "@clerk/nextjs";
 import { UIState } from "../store";
 
 export default function MobileFooter() {
+  const { user } = useUser();
+
+  const createClerkPasskey = async () => {
+    try {
+      const response = await user?.createPasskey();
+      console.log(response);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   const toggleDrawer = useToggleDrawer();
   const isDrawerOpen = useStore((state: UIState) => state.isDrawerOpen);
 
@@ -30,10 +42,46 @@ export default function MobileFooter() {
         <FaShoppingBag size={24} />
         <span className="text-xs mt-1">Basket</span>
       </button>
-      <button className="flex flex-col items-center">
+      {/* <button className="flex flex-col items-center">
         <FaUser size={24} />
         <span className="text-xs mt-1">Account</span>
-      </button>
+      </button> */}
+      <ClerkLoaded>
+        <div>
+          {user ? (
+            <div className="mt-2 flex flex-col justify-center">
+              <div className="mb-1 flex justify-center items-center">
+                <UserButton />
+
+                {user?.passkeys.length === 0 && (
+                  <div className="ml-2 flex items-center justify-center">
+                    <button
+                      onClick={createClerkPasskey}
+                      className="h-[28px] w-[28px] flex justify-center items-center bg-white hover:bg-white-700 hover:text-white animate-pulse text-black font-bold p-2 rounded-full border-blue-300 border"
+                    >
+                      <FaKey />
+                    </button>
+                  </div>
+                )}
+              </div>
+              <div
+                style={{ lineHeight: "16px" }}
+                className="flex items-center justify-center"
+              >
+                <p className="text-white">Welcome back</p>
+                <span className="font-bold text-white">{user.fullName}</span>
+              </div>
+            </div>
+          ) : (
+            <div className="flex flex-col justify-center items-center">
+              <FaUser className="w-[24px] h-[24px] text-white outline-none" />
+              <div className="text-white text-xs mt-1">
+                <SignInButton mode="modal" />
+              </div>
+            </div>
+          )}
+        </div>
+      </ClerkLoaded>
     </div>
   );
 }
