ed677d0bbb979fe6b8d9b6a1f7f4c2c0b746235b
Munrhalls <antarcticdepths71@gmail.com>
2024-11-27 17:56:27 +0100

    nice anim

--- components/MobileFooter.tsx
+++ components/MobileFooter.tsx
@@ -1,10 +1,13 @@
 "use client";
 
 import { FaBars, FaSearch, FaShoppingBag, FaUser } from "react-icons/fa";
-import { useToggleDrawer } from "../store";
+import { useToggleDrawer, useStore } from "../store";
+
+import { UIState } from "../store";
 
 export default function MobileFooter() {
   const toggleDrawer = useToggleDrawer();
+  const isDrawerOpen = useStore((state: UIState) => state.isDrawerOpen);
 
   return (
     <div className="fixed bottom-0 left-0 right-0 bg-black text-white flex justify-around items-center h-16 lg:hidden">
@@ -12,7 +15,14 @@ export default function MobileFooter() {
         <FaBars size={24} />
         <span className="text-xs mt-1">Categories</span>
       </button>
-      <button className="flex flex-col items-center" onClick={toggleDrawer}>
+      <button
+        className={`flex flex-col items-center transition-transform duration-150 ${
+          isDrawerOpen
+            ? "font-bold shadow-lg bg-white text-black px-4 py-2 rounded"
+            : ""
+        }`}
+        onClick={toggleDrawer}
+      >
         <FaSearch size={24} />
         <span className="text-xs mt-1">Search</span>
       </button>
--- store.ts
+++ store.ts
@@ -1,15 +1,13 @@
 import { create } from "zustand";
 
-interface UIState {
+export interface UIState {
   isDrawerOpen: boolean;
   toggleDrawer: () => void;
 }
 
-const useStore = create<UIState>((set) => ({
+export const useStore = create<UIState>((set) => ({
   isDrawerOpen: false,
   toggleDrawer: () => set((state) => ({ isDrawerOpen: !state.isDrawerOpen })),
 }));
 
 export const useToggleDrawer = () => useStore((state) => state.toggleDrawer);
-
-export default useStore;
