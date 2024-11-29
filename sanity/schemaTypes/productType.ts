5aeb8480169f93ad68bcc33ea7688d02b6fd3034
Munrhalls <antarcticdepths71@gmail.com>
2024-11-29 17:08:07 +0100

    fix categories

--- sanity/schemaTypes/productType.ts
+++ sanity/schemaTypes/productType.ts
@@ -58,6 +58,7 @@ export const productType = defineType({
           name: "ref",
           type: "reference",
           to: [{ type: "category" }],
+          weak: true,
         },
       ],
       validation: (Rule) => Rule.required(),
