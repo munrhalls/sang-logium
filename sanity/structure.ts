97f7c6d356242124d2613d7c4994f8931715e988
Munrhalls <antarcticdepths71@gmail.com>
2024-12-06 12:21:01 +0100

    categories, SANITY STUDIO HIERARCHICAL REPRESENTATION

--- sanity/structure.ts
+++ sanity/structure.ts
@@ -1,11 +1,25 @@
-import type { StructureResolver } from "sanity/structure";
+import type { StructureResolver, StructureBuilder } from "sanity/structure";
+
+const categoryChildResolver = (S: StructureBuilder, id: string) => {
+  return S.documentList()
+    .title("Subcategories")
+    .filter('_type == "category" && parentCategory._ref == $parentId')
+    .params({ parentId: id });
+};
 
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
   S.list()
     .title("Sang Logium E-commerce Web App")
     .items([
-      S.documentTypeListItem("category").title("Categories"),
+      S.documentTypeListItem("category")
+        .title("Categories")
+        .child(
+          S.documentTypeList("category")
+            .title("Categories")
+            .filter('_type == "category" && !defined(parentCategory)')
+            .child((id) => categoryChildResolver(S, id))
+        ),
 
       S.divider(),
       ...S.documentTypeListItems().filter(
