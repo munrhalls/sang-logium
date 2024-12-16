058e99df035d857f0a563eae9a7578fcfea383d6
Munrhalls <antarcticdepths71@gmail.com>
2024-12-16 15:10:44 +0100

    Mobile categories drawer - inner sticky sticks and fits well, inner content scrolls and respects the sticky

--- components/MobileSearchDrawer.tsx
+++ components/MobileSearchDrawer.tsx
@@ -2,6 +2,7 @@
 
 import { useStore } from "../store";
 import { useEffect, useState } from "react";
+import { FaTimes } from "react-icons/fa";
 
 export default function MobileSearchDrawer() {
   const isSearchDrawerOpen = useStore((state) => state.isSearchDrawerOpen);
@@ -20,16 +21,28 @@ export default function MobileSearchDrawer() {
 
   return (
     <div
-      className={`z-50 pointer-events-auto fixed top-[60px] left-0 bottom-[60px] bg-white text-black transition-transform duration-300 ${
+      className={`z-50 pointer-events-auto fixed top-0 left-0 bg-white text-black transition-transform duration-300 ${
         isSearchDrawerOpen ? "translate-x-0" : "-translate-x-full"
       }`}
-      style={{ width: "90%" }}
+      style={{
+        marginTop: "var(--header-height)",
+        // marginBottom: "var(--footer-height)",
+        // height: "calc(100vh - var(--header-height) - var(--footer-height))",
+        width: "90%",
+      }}
     >
       <div className="p-4">
-        <div className="flex justify-end items-center mr-2">
-          <button onClick={toggleSearchDrawer} className="text-black">
-            Close
-          </button>
+        <div className="p-2 border-b border-gray-200">
+          <div className="flex justify-end items-center">
+            <button
+              onClick={toggleSearchDrawer}
+              className="flex gap-1 items-center justify-center text-black"
+            >
+              <span>CLOSE</span>
+
+              <FaTimes size={14} />
+            </button>
+          </div>
         </div>
 
         <form action="/search" className="mt-4">
