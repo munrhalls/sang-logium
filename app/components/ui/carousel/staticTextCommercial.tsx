10d84924952983e2fb1784592cb180ccb25ce810
Munrhalls <antarcticdepths71@gmail.com>
2025-01-23 22:03:54 +0100

    updating commercials schema, updating prod commercials to include text - stylin rwd - stylin text commercial

--- app/components/ui/carousel/staticTextCommercial.tsx
+++ app/components/ui/carousel/staticTextCommercial.tsx
@@ -10,6 +10,7 @@ type CommercialText = NonNullable<
 interface TextCommercialProps {
   text: CommercialText;
 }
+
 export default function TextCommercial({ text }: TextCommercialProps) {
   const buttonColor =
     text[0]?.markDefs?.find((mark) => mark._type === "textColor")?.value ||
@@ -17,8 +18,12 @@ export default function TextCommercial({ text }: TextCommercialProps) {
 
   const components: PortableTextComponents = {
     block: {
-      h1: ({ children }) => <h1 className="text-2xl font-black">{children}</h1>,
-      h2: ({ children }) => <h2 className="text-xl font-bold">{children}</h2>,
+      h1: ({ children }) => (
+        <h1 className="text-2xl lg:text-3xl font-black">{children}</h1>
+      ),
+      h2: ({ children }) => (
+        <h2 className="text-xl lg:text-2xl font-bold">{children}</h2>
+      ),
       normal: ({ children }) => <p className="text-xl">{children}</p>,
     },
     marks: {
@@ -29,18 +34,20 @@ export default function TextCommercial({ text }: TextCommercialProps) {
   };
 
   return (
-    <div className="/* TEXT COMMERCIAL */ bg-black/40 text-white grid ">
-      <div className="z-20">
+    <div className="/* TEXT COMMERCIAL */ h-full  z-30 text-white grid place-content-center">
+      <div className="z-20 bg-black/40 grid place-items-center p-12  space-y-1 md:space-y-4 rounded-sm">
         <PortableText value={text} components={components} />;
       </div>
-      <Link
-        href="asdasaxzc"
-        prefetch={true}
-        className="z-20 h-full block max-w-[10rem] text-center text-xl text-white font-black px-6 py-2 rounded-sm mt-4 md:mt-12"
-        style={{ backgroundColor: `${buttonColor}` }}
-      >
-        SEE MORE
-      </Link>
+      <div className="z-20 grid place-items-center">
+        <Link
+          href="asdasaxzc"
+          prefetch={true}
+          className="z-20 block max-w-[10rem] text-center text-xl text-white font-black px-6 py-2 rounded-sm mt-4 md:mt-12"
+          style={{ backgroundColor: `${buttonColor}` }}
+        >
+          SEE MORE
+        </Link>
+      </div>
     </div>
   );
 }
