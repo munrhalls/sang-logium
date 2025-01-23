e5d9af0615a8ddd6efd7e1cb97177818a13a9fde
Munrhalls <antarcticdepths71@gmail.com>
2025-01-23 21:49:02 +0100

    updating commercials schema, updating prod commercials to include text - stylin rwd

--- app/components/ui/carousel/staticProductsCommercial.tsx
+++ app/components/ui/carousel/staticProductsCommercial.tsx
@@ -1,3 +1,5 @@
+import { PortableText } from "@portabletext/react";
+import { PortableTextComponents } from "@portabletext/react";
 import { ProductProps, ProductCard } from "./staticProductCard";
 import Link from "next/link";
 import { GET_COMMERCIALS_BY_FEATURE_QUERYResult } from "@/sanity.types";
@@ -17,9 +19,28 @@ function ProductsCommercial({
     text?.[0]?.markDefs?.find((mark) => mark._type === "textColor")?.value ||
     "#CF8226";
 
+  const components: PortableTextComponents = {
+    block: {
+      h1: ({ children }) => <h1 className="text-2xl font-black">{children}</h1>,
+      h2: ({ children }) => <h2 className="text-xl font-bold">{children}</h2>,
+      normal: ({ children }) => <p className="text-xl">{children}</p>,
+    },
+    marks: {
+      textColor: ({ value, children }) => (
+        <span style={{ color: value?.value || "inherit" }}>{children}</span>
+      ),
+    },
+  };
+
   return (
-    <div className="z-30 h-full w-full grid grid-rows-[3rem_1fr_3rem] p-4 gap-3 lg:auto-cols-fr">
-      <div className="h-full bg-green">hey</div>
+    <div className="z-30 h-full w-full grid grid-rows-[auto_1fr_3rem] p-4 gap-3 lg:auto-cols-fr">
+      <div className="h-full bg-green bg-black/40 text-white grid ">
+        {text && (
+          <div className="z-20">
+            <PortableText value={text} components={components} />
+          </div>
+        )}
+      </div>
       <div className="h-full grid">
         {products.map((product) => (
           <ProductCard
