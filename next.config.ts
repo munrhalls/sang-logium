1d2c31b13af2702bad412280ca5c9d74285eb55e
Munrhalls <antarcticdepths71@gmail.com>
2025-01-25 22:13:17 +0100

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,60 +1,10 @@
-import {withSentryConfig} from "@sentry/nextjs";
+import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // experimental: {
-  //   turbo: {
-  //     treeShaking: true,
-  //     rules: {
-  //       "*.svg": {
-  //         loaders: ["@svgr/webpack"],
-  //         as: "*.js",
-  //       },
-  //     },
-  //   },
-  // },
-
-  // webpack: (config, { dev, isServer }) => {
-  //   config.optimization.splitChunks = {
-  //     chunks: "all",
-  //     minSize: 20000,
-  //     maxSize: 70000,
-  //     cacheGroups: {
-  //       commons: {
-  //         test: /[\\/]node_modules[\\/]/,
-  //         name: "vendors",
-  //         chunks: "all",
-  //       },
-  //     },
-  //   };
-  //   if (!dev && !isServer) {
-  //     config.optimization.usedExports = true;
-  //   }
-
-  //   // const fileLoaderRule = config.module.rules.find(
-  //   //   (rule: RuleSetRule) =>
-  //   //     rule.test instanceof RegExp && rule.test.test(".svg")
-  //   // );
-
-  //   // config.module.rules.push(
-  //   //   {
-  //   //     ...fileLoaderRule,
-  //   //     test: /\.svg$/i,
-  //   //     resourceQuery: /url/, // *.svg?url
-  //   //   },
-  //   //   {
-  //   //     test: /\.svg$/i,
-  //   //     issuer: fileLoaderRule.issuer,
-  //   //     resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
-  //   //     use: ["@svgr/webpack"],
-  //   //   }
-  //   // );
-
-  //   // fileLoaderRule.exclude = /\.svg$/i;
-
-  //   return config;
-  // },
-
+  paths: {
+    "@/*": ["./*"],
+  },
   typescript: {
     ignoreBuildErrors: false,
   },
@@ -83,41 +33,41 @@ const nextConfig: NextConfig = {
 };
 
 export default withSentryConfig(nextConfig, {
-// For all available options, see:
-// https://github.com/getsentry/sentry-webpack-plugin#options
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options
 
-org: "j-b3c8e5abd",
-project: "javascript-nextjs",
+  org: "j-b3c8e5abd",
+  project: "javascript-nextjs",
 
-// Only print logs for uploading source maps in CI
-silent: !process.env.CI,
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
 
-// For all available options, see:
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+  // For all available options, see:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-// Upload a larger set of source maps for prettier stack traces (increases build time)
-widenClientFileUpload: true,
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
 
-// Automatically annotate React components to show their full name in breadcrumbs and session replay
-reactComponentAnnotation: {
-enabled: true,
-},
+  // Automatically annotate React components to show their full name in breadcrumbs and session replay
+  reactComponentAnnotation: {
+    enabled: true,
+  },
 
-// Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-// This can increase your server load as well as your hosting bill.
-// Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-// side errors will fail.
-// tunnelRoute: "/monitoring",
+  // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+  // This can increase your server load as well as your hosting bill.
+  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+  // side errors will fail.
+  // tunnelRoute: "/monitoring",
 
-// Hides source maps from generated client bundles
-hideSourceMaps: true,
+  // Hides source maps from generated client bundles
+  hideSourceMaps: true,
 
-// Automatically tree-shake Sentry logger statements to reduce bundle size
-disableLogger: true,
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
 
-// Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-// See the following for more information:
-// https://docs.sentry.io/product/crons/
-// https://vercel.com/docs/cron-jobs
-automaticVercelMonitors: true,
+  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+  // See the following for more information:
+  // https://docs.sentry.io/product/crons/
+  // https://vercel.com/docs/cron-jobs
+  automaticVercelMonitors: true,
 });
