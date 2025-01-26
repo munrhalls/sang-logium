fa15612784651843f1087d09e4c7f71583851cfa
Munrhalls <antarcticdepths71@gmail.com>
2025-01-26 01:10:54 +0100

    fuck

--- app/components/features/brands-wall/BrandsWall.tsx
+++ app/components/features/brands-wall/BrandsWall.tsx
@@ -3,8 +3,6 @@ import * as Brands from "@/public/brands";
 import LogoOrbit from "@/public/logo-orbit.svg";
 
 export default async function BrandsWall() {
-  console.log(Object.keys(Brands).map((brand) => brand));
-  console.log(Brands);
   return (
     <div className="h-full grid grid-cols-[1fr_5fr_1fr]">
       <div className="h-full pt-8 pb-4 col-start-2 col-end-3 flex justify-center items-center gap-4 gradient">
