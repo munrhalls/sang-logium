143c1ea7dcc8b03eb56018560a6afce7d493aaa8
Munrhalls <antarcticdepths71@gmail.com>
2025-01-26 01:09:29 +0100

    fuck

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,15 +1,12 @@
 "use client";
 
 import NextError from "next/error";
-import { useEffect } from "react";
 
 export default function GlobalError({
   error,
 }: {
   error: Error & { digest?: string };
 }) {
-  useEffect(() => {}, [error]);
-
   return (
     <html>
       <body>
@@ -17,6 +14,7 @@ export default function GlobalError({
         definition requires a `statusCode` prop. However, since the App Router
         does not expose status codes for errors, we simply pass 0 to render a
         generic error message. */}
+        {error.message}
         <NextError statusCode={0} />
       </body>
     </html>
