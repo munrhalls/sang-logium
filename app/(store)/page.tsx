26bf4253d67800893d664f67a1147875d7b10f8a
Munrhalls <antarcticdepths71@gmail.com>
2025-01-27 16:56:19 +0100

    try no carousel

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,6 +1,6 @@
-import { getCommercialsByFeature } from "@/sanity/lib/commercials/getCommercialsByFeature";
-import Carousel from "@/app/components/ui/carousel/carousel";
-import Slide from "@/app/components/ui/carousel/staticSlide";
+// import { getCommercialsByFeature } from "@/sanity/lib/commercials/getCommercialsByFeature";
+// import Carousel from "@/app/components/ui/carousel/carousel";
+// import Slide from "@/app/components/ui/carousel/staticSlide";
 import BrandsWall from "../components/features/brands-wall/BrandsWall";
 import ExtremeQuality from "../components/features/extreme-quality/ExtremeQuality";
 import Bestsellers from "../components/features/bestsellers/Bestsellers";
@@ -8,22 +8,22 @@ import MonthMvp from "../components/features/month-mvp/MonthMvp";
 // import MobileFooter from "@/app/components/ui/footer/MobileFooter";
 
 export default async function Page() {
-  const heroCommercials = await getCommercialsByFeature("hero");
+  // const heroCommercials = await getCommercialsByFeature("hero");
   // const categories = await getAllCategories();
 
-  const prebuiltSlides = heroCommercials
-    .sort((a, b) => (a?.displayOrder ?? 0) - (b?.displayOrder ?? 0))
-    .map((commercial, index) => (
-      <Slide key={commercial._id} commercial={commercial} index={index} />
-    ));
+  // const prebuiltSlides = heroCommercials
+  //   .sort((a, b) => (a?.displayOrder ?? 0) - (b?.displayOrder ?? 0))
+  //   .map((commercial, index) => (
+  //     <Slide key={commercial._id} commercial={commercial} index={index} />
+  //   ));
 
-  const heroCommercialsKeys = heroCommercials.map(
-    (commercial) => commercial._id
-  );
+  // const heroCommercialsKeys = heroCommercials.map(
+  //   (commercial) => commercial._id
+  // );
 
   return (
     <main className="h-full ">
-      <Carousel prebuiltSlides={prebuiltSlides} keys={heroCommercialsKeys} />
+      {/* <Carousel prebuiltSlides={prebuiltSlides} keys={heroCommercialsKeys} /> */}
       <div className="">
         <BrandsWall />
       </div>
