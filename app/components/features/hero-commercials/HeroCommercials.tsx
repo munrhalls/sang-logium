67651e9ce5ae5ebd978f68e1e5342f5f4562c999
Munrhalls <antarcticdepths71@gmail.com>
2025-01-30 19:50:47 +0100

    remake

---
+++ app/components/features/hero-commercials/HeroCommercials.tsx
@@ -0,0 +1,21 @@
+import { getCommercialsByFeature } from "@/sanity/lib/commercials/getCommercialsByFeature";
+import Carousel from "../../ui/carousel/carousel";
+import Slide from "../../ui/carousel/staticSlide";
+
+export default async function HeroCommercials() {
+  const heroCommercials = await getCommercialsByFeature("hero");
+
+  const prebuiltSlides = heroCommercials
+    .sort((a, b) => (a?.displayOrder ?? 0) - (b?.displayOrder ?? 0))
+    .map((commercial, index) => (
+      <Slide key={commercial._id} commercial={commercial} index={index} />
+    ));
+
+  const heroCommercialsKeys = heroCommercials.map(
+    (commercial) => commercial._id
+  );
+
+  return (
+    <Carousel prebuiltSlides={prebuiltSlides} keys={heroCommercialsKeys} />
+  );
+}
