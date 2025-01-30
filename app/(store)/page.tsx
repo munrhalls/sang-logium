7224f921e6b7d0d9c3f917ac40e59aeb015cf932
Munrhalls <antarcticdepths71@gmail.com>
2025-01-30 19:51:07 +0100

    remake

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,6 +1,3 @@
-import { getCommercialsByFeature } from "@/sanity/lib/commercials/getCommercialsByFeature";
-import Carousel from "@/app/components/ui/carousel/carousel";
-import Slide from "@/app/components/ui/carousel/staticSlide";
 import BrandsWall from "../components/features/brands-wall/BrandsWall";
 import ExtremeQuality from "../components/features/extreme-quality/ExtremeQuality";
 import Bestsellers from "../components/features/bestsellers/Bestsellers";
@@ -8,27 +5,14 @@ import MonthProduct from "../components/features/month-product/MonthProduct";
 import NewestRelease from "../components/features/newest-release/NewestRelease";
 import FeaturedProducts from "../components/features/featured-products/FeaturedProducts";
 import MainCategories from "../components/features/main-categories/MainCategories";
-// import MobileFooter from "@/app/components/ui/footer/MobileFooter";
 import { Suspense } from "react";
 import Footer from "@/app/components/layout/footer/Footer";
 
 export default async function Page() {
-  const heroCommercials = await getCommercialsByFeature("hero");
   // const categories = await getAllCategories();
-
-  const prebuiltSlides = heroCommercials
-    .sort((a, b) => (a?.displayOrder ?? 0) - (b?.displayOrder ?? 0))
-    .map((commercial, index) => (
-      <Slide key={commercial._id} commercial={commercial} index={index} />
-    ));
-
-  const heroCommercialsKeys = heroCommercials.map(
-    (commercial) => commercial._id
-  );
 
   return (
     <main className="h-full ">
-      <Carousel prebuiltSlides={prebuiltSlides} keys={heroCommercialsKeys} />
       <Suspense
         fallback={
           <div className="h-full grid place-content-center">Loading...</div>
@@ -36,6 +20,7 @@ export default async function Page() {
       >
         <BrandsWall />
       </Suspense>
+
       <Bestsellers />
       <MonthProduct />
       <ExtremeQuality />
