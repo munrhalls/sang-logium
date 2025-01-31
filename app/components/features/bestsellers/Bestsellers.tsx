8eb9fd032e157c6e8c541bf6c26c6c128049070e
Munrhalls <antarcticdepths71@gmail.com>
2025-01-31 22:13:47 +0100

    scaffold

--- app/components/features/bestsellers/Bestsellers.tsx
+++ app/components/features/bestsellers/Bestsellers.tsx
@@ -10,9 +10,9 @@ export default async function Bestsellers() {
       return (
         <div
           key={index + "_Bestsellers"}
-          className="h-full w-full p-4 grid relative bg-purple-700 border border-black"
+          className="h-full w-full p-4 grid place-items-center relative bg-purple-700 border border-black"
         >
-          <div className="h-full bg-orange-700 grid grid-rows-[auto_2fr_auto]">
+          <div className="h-full w-full max-w-[300px] bg-orange-700 grid grid-rows-[auto_2fr_auto]">
             <div className="">brand {index}</div>
             <div className="h-full bg-teal-700">image</div>
             <div>price</div>
