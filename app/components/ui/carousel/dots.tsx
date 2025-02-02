88685a1959e8e7f5dae96a986a987a5665d50ac6
Munrhalls <antarcticdepths71@gmail.com>
2025-02-02 18:45:44 +0100

    refactored multi slide carousel to use pure css

--- app/components/ui/carousel/dots.tsx
+++ app/components/ui/carousel/dots.tsx
@@ -14,36 +14,38 @@ export default function Dots({
 }: CarouselDotsProps) {
   const numberOfDots = keys.length;
   const [height, width] = numberOfDots > 10 ? [8, 8] : [16, 16];
+  const w = true;
 
   return (
     <div
-      className={` z-40 h-full truncate text-white text-center sm:bg-transparent flex flex-shrink-0 justify-center items-center ${numberOfDots > 10 ? "gap-[0.25rem]" : "gap-2"} lg:gap-3 xl:gap-4 xl:mb-4 cursor-pointer`}
+      className={`z-40 h-full truncate text-white text-center sm:bg-transparent flex flex-shrink-0 justify-center items-center ${numberOfDots > 10 ? "gap-[0.25rem]" : "gap-2"} lg:gap-3 xl:gap-4 xl:mb-4`}
     >
       {keys.map((key, i) => {
-        return currentIndex === i ? (
-          <Image
-            loading="lazy"
-            key={key}
-            src={Logo}
-            height={height + 8}
-            width={width + 8}
-            alt={"Icon"}
-            unoptimized
-            onClick={() => onDotClick(i)}
-            className="cursor-pointer"
-          />
-        ) : (
-          <Image
-            loading="lazy"
-            key={key}
-            src={Ellipse}
-            height={height}
-            width={width}
-            alt={"Icon"}
-            unoptimized
-            onClick={() => onDotClick(i)}
-            className="cursor-pointer"
-          />
+        return (
+          <>
+            <Image
+              loading="lazy"
+              key={key}
+              src={Logo}
+              height={height + 8}
+              width={width + 8}
+              alt={"Icon"}
+              unoptimized
+              onClick={() => onDotClick(i)}
+              className={` ${currentIndex === i ? "" : "hidden"}`}
+            />
+            <Image
+              loading="lazy"
+              key={key}
+              src={Ellipse}
+              height={height}
+              width={width}
+              alt={"Icon"}
+              unoptimized
+              onClick={() => onDotClick(i)}
+              className={`cursor-pointer ${currentIndex === i ? "hidden" : ""}`}
+            />
+          </>
         );
       })}
     </div>
