a393a6a217f34ef5e31bdf428a601c3ad280c5f1
Munrhalls <antarcticdepths71@gmail.com>
2025-02-03 13:26:08 +0100

    layout

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -15,15 +15,15 @@ export default async function Page() {
     <main className="h-full">
       <HeroCommercials />
 
-      <div className="grid grid-cols-[auto_5fr_auto] md:grid-cols-[1fr_5fr_1fr] mx-auto">
-        <div className="col-start-2 col-end-3 max-w-[1400px]">
+      <div className="grid grid-cols-[auto_8fr_auto] xl:grid-cols-[1fr_8fr_1fr] mx-auto">
+        <div className="mx-auto col-start-2 col-end-3 max-w-[1400px]">
           <BrandsWall />
           <Bestsellers />
         </div>
         <div className="col-start-1 col-end-4">
           <NewestRelease />
         </div>
-        <div className="col-start-2 col-end-3 max-w-[1400px]">
+        <div className="mx-auto col-start-2 col-end-3 max-w-[1400px]">
           <ExtremeQuality />
           <FeaturedProducts />
         </div>
