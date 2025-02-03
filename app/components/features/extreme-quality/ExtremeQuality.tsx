bbd87f55d03516a0c8e117986bfac2e35001c027
Munrhalls <antarcticdepths71@gmail.com>
2025-02-03 11:29:08 +0100

    minor

--- app/components/features/extreme-quality/ExtremeQuality.tsx
+++ app/components/features/extreme-quality/ExtremeQuality.tsx
@@ -1,20 +1,32 @@
 import Carousel from "../../ui/carousel-single-slide/carouselSingleSlide";
 import SegmentTitle from "../../ui/segment-title/SegmentTitle";
+import BrandTitle from "../../ui/commercials/minor/brandTitle";
+import Price from "../../ui/commercials/minor/price";
 
 export default async function ExtremeQuality() {
   const keys: string[] = [];
+  const image = { width: 500, height: 500, title: "Square (1:1)" };
+
   const prebuiltCommercials = Array.from({ length: 3 }).map(
     (_, index: number) => {
       keys.push(`bestseller_${index}`);
       return (
         <div
           key={index + "_Bestsellers"}
-          className="h-full w-full p-4 grid place-items-center relative  border border-black"
+          className="h-full w-full p-4 grid place-items-center relative  border border-black bg-slate-400"
         >
           <div className="h-full w-full max-w-[300px]  grid grid-rows-[auto_2fr_auto]">
-            <div className="">brand {index}</div>
-            <div className="h-full ">image</div>
-            <div>price</div>
+            <BrandTitle brand={"Sony"} />
+            <div className="h-full ">
+              <img
+                src={`https://picsum.photos/${image.width}/${image.height}`}
+                height={image.height}
+                width={image.width}
+                alt={image.title}
+                className="w-full h-full object-cover rounded-sm"
+              />
+            </div>
+            <Price price={2599.99} priceColor={"blue"} />
           </div>
         </div>
       );
--- app/components/features/month-product/MonthProduct.tsx
+++ app/components/features/month-product/MonthProduct.tsx
@@ -2,6 +2,7 @@
 import SegmentTitle from "../../ui/segment-title/SegmentTitle";
 import { useEffect } from "react";
 import { useState } from "react";
+// import { ProductCard } from "../../ui/commercials/productCard";
 
 export default function MonthProduct() {
   const [timeLeft, setTimeLeft] = useState({
@@ -44,11 +45,11 @@ export default function MonthProduct() {
   }, []);
 
   return (
-    <div className="h-full w-full  grid grid-rows-[5rem_4fr]">
+    <div className="h-full w-full  grid grid-rows-[5rem_4fr] bg-slate-300">
       <SegmentTitle title="MVP of the month!" />
-      <div className="h-full w-full  grid grid-rows-[5rem_1fr] md:grid-rows-1 md:grid-cols-[1fr_2fr] gap-1">
+      <div className="h-full w-full grid grid-rows-[5rem_1fr] md:grid-rows-1 md:grid-cols-[1fr_2fr] gap-1">
         <div className="h-full font-black text-xl grid  grid-rows-[2rem_3rem] md:place-content-center md:mb-4">
-          <div className="text-white text-center">Time left</div>
+          <div className="text-black text-center">Time left</div>
           <div className="grid place-content-center grid-cols-[3rem_3rem_3rem] gap-1">
             <div className="text-center">
               <div>{timeLeft.days}:</div>
@@ -64,9 +65,9 @@ export default function MonthProduct() {
             </div>
           </div>
         </div>
-        <div className="bg-green-300 h-full p-8 grid">
+        <div className=" h-full p-8 grid">
           <div className="bg-white h-full grid place-content-center rounded-xl">
-            <span>product</span>
+            {/* <ProductCard /> */}
           </div>
         </div>
       </div>
