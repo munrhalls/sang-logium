e4f615ec180d8464ef33ab33bfa5318559f017e8
Munrhalls <antarcticdepths71@gmail.com>
2025-02-05 23:49:36 +0100

    layout mvp

--- app/components/features/month-product/MonthProduct.tsx
+++ app/components/features/month-product/MonthProduct.tsx
@@ -1,16 +1,10 @@
 "use client";
-import SegmentTitle from "../../ui/segment-title/SegmentTitle";
 import { useEffect } from "react";
 import { useState } from "react";
-// import { ProductCard } from "../../ui/commercials/productCard";
+import LogoOrbitWhite from "@/public/logo-orbit-white.svg";
+import Image from "next/image";
 
-export default function MonthProduct() {
-  const [timeLeft, setTimeLeft] = useState({
-    days: "00",
-    hours: "00",
-    minutes: "00",
-  });
-
+const TimeStamp = function () {
   useEffect(() => {
     const calculateTimeLeft = () => {
       const now = new Date();
@@ -44,33 +38,103 @@ export default function MonthProduct() {
     return () => clearInterval(timer);
   }, []);
 
+  const [timeLeft, setTimeLeft] = useState({
+    days: "00",
+    hours: "00",
+    minutes: "00",
+  });
+
   return (
-    <div className="min-h-[800px] w-full  grid grid-rows-[5rem_4fr] bg-black">
-      <SegmentTitle title="MVP of the month!" white={true} />
-      <div className="h-full w-full grid grid-rows-[5rem_1fr] md:grid-rows-1 md:grid-cols-[1fr_2fr] gap-1">
-        <div className="h-full font-black text-xl grid  grid-rows-[2rem_3rem] md:place-content-center md:mb-4">
-          <div className="text-white text-center">Time left</div>
-          <div className="grid place-content-center grid-cols-[3rem_3rem_3rem] gap-1 text-white">
-            <div className="text-center">
-              <div>{timeLeft.days}:</div>
-              <div className="text-sm">days</div>
-            </div>
-            <div className="text-center">
-              <div>{timeLeft.hours}:</div>
-              <div className="text-sm">hours</div>
-            </div>
-            <div className="text-center">
-              <div>{timeLeft.minutes}</div>
-              <div className="text-sm">minutes</div>
-            </div>
-          </div>
+    <div className="h-full font-black text-xl grid place-content-center grid-rows-[2rem_3rem]">
+      <div className="text-white text-center">Time left</div>
+      <div className="grid place-content-center grid-cols-[3rem_3rem_3rem] gap-1 text-white">
+        <div className="text-center">
+          <div>{timeLeft.days}:</div>
+          <div className="text-sm">days</div>
         </div>
-        <div className=" h-full p-8 grid">
-          <div className="bg-white h-full grid place-content-center rounded-xl">
-            {/* <ProductCard /> */}
-          </div>
+        <div className="text-center">
+          <div>{timeLeft.hours}:</div>
+          <div className="text-sm">hours</div>
+        </div>
+        <div className="text-center">
+          <div>{timeLeft.minutes}</div>
+          <div className="text-sm">minutes</div>
         </div>
       </div>
     </div>
   );
+};
+
+const Title = function () {
+  return (
+    <div className="p-4 grid gap-2">
+      <div className="flex gap-2">
+        <Image
+          src={LogoOrbitWhite}
+          alt="Logo"
+          height={24}
+          width={24}
+          loading="lazy"
+        />
+        <h1 className="text-2xl">Product of the month</h1>
+      </div>
+      <p>Produkt Dnia Smartfon APPLE iPhone 13 128GB Północ MLPF3PM/A</p>
+    </div>
+  );
+};
+
+const CTA = function () {
+  return (
+    <div className="h-full grid content-center items-center">
+      <button className="bg-white text-black font-bold p-2 rounded-lg">
+        Shop now
+      </button>
+    </div>
+  );
+};
+
+export default function MonthProduct() {
+  return (
+    <div className="h-[800px] bg-black grid md:grid-cols-2">
+      <div className="bg-orange-700 md:row-start-5 md:col-start-1 md:col-span-1">
+        <TimeStamp />
+      </div>
+      <div className="bg-green-700 grid content-center justify-start 2xs:justify-center text-white row-start-4 md:row-start-4 md:col-start-1 md:col-span-2">
+        <Title />
+      </div>
+      <div className="bg-yellow-700  row-start-5 grid place-content-center md:col-start-2 md:col-span-1">
+        <CTA />
+      </div>
+      <div className="bg-blue-950 grid p-12 md:col-start-1 md:col-span-2">
+        <div className="bg-white rounded-xl min-h-[300px]"></div>
+      </div>
+    </div>
+  );
 }
+
+//  <div className="min-h-[800px] w-full  grid grid-rows-[5rem_4fr] bg-black">
+//   <div className="h-full w-full grid grid-rows-[5rem_1fr] md:grid-rows-1 md:grid-cols-[1fr_2fr] gap-1">
+//     <div className="h-full font-black text-xl grid  grid-rows-[2rem_3rem_3rem] md:place-content-center md:mb-4">
+//       <div className="text-white text-center">Time left</div>
+//       <div className="grid place-content-center grid-cols-[3rem_3rem_3rem] gap-1 text-white">
+//         <div className="text-center">
+//           <div>{timeLeft.days}:</div>
+//           <div className="text-sm">days</div>
+//         </div>
+//         <div className="text-center">
+//           <div>{timeLeft.hours}:</div>
+//           <div className="text-sm">hours</div>
+//         </div>
+//         <div className="text-center">
+//           <div>{timeLeft.minutes}</div>
+//           <div className="text-sm">minutes</div>
+//         </div>
+//       </div>
+//     </div>
+//     <div className="h-full p-8 grid">
+//       <div className="bg-white h-full grid place-content-center rounded-xl">
+//          <ProductCard />
+//       </div>
+//     </div>
+//   </div>
+// </div>;
--- app/components/ui/segment-title/SegmentTitle.tsx
+++ app/components/ui/segment-title/SegmentTitle.tsx
@@ -4,16 +4,26 @@ import LogoOrbitWhite from "@/public/logo-orbit-white.svg";
 
 export default function SegmentTitle({
   title,
+  width,
+  height,
   white = false,
 }: {
   title: string;
   white?: boolean;
+  width?: number;
+  height?: number;
 }) {
   const Logo = white ? LogoOrbitWhite : LogoOrbit;
 
   return (
-    <div className="flex items-center justify-center gap-1">
-      <Image src={Logo} alt="Logo" width={60} height={60} unoptimized />
+    <div className={`flex items-center justify-center gap-1`}>
+      <Image
+        src={Logo}
+        alt="Logo"
+        width={width ?? 60}
+        height={height ?? 60}
+        unoptimized
+      />
       <h1
         className={`${white ? "text-white" : "text-black"} font-black text-3xl`}
       >
