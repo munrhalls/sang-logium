75bfbb8bd08948ca4ca741d58bca0accdefbb9ad
Munrhalls <antarcticdepths71@gmail.com>
2025-02-07 09:00:23 +0100

    zustand state

--- app/components/layout/mobile/MobileMenu.tsx
+++ app/components/layout/mobile/MobileMenu.tsx
@@ -1,13 +1,27 @@
+"use client";
 import React from "react";
 import { Menu, Search, ShoppingBag, User } from "lucide-react";
+import { useStore, UIState } from "@/store";
 
 const MobileMenu = () => {
+  const isCategoriesOpen = useStore((state) => state.isCategoriesDrawerOpen);
+  const toggleCategoriesDrawer = useStore(
+    (state) => state.toggleCategoriesDrawer
+  );
+
   return (
     <div className=" bg-black text-white border-t border-white lg:hidden">
       <div className="h-16 flex justify-around items-center px-4">
-        <button className="flex flex-col items-center">
+        <button
+          className="flex flex-col items-center"
+          onClick={toggleCategoriesDrawer}
+        >
           <Menu className="h-6 w-6" />
-          <span className="text-xs mt-1">Categories</span>
+          {isCategoriesOpen ? (
+            <span className="text-xs mt-1">X</span>
+          ) : (
+            <span className="text-xs mt-1">Menu</span>
+          )}
         </button>
 
         <button className="flex flex-col items-center">
