dce11fc13e9f66ef4a0ae90c1f37d776b64e662a
Munrhalls <antarcticdepths71@gmail.com>
2025-02-07 23:07:50 +0100

    modify DNS, BIG CHANGES

---
+++ auth.ts
@@ -0,0 +1,7 @@
+export const authConfig = {
+  signInUrl: "/sign-in",
+  signUpUrl: "/sign-up",
+  afterSignInUrl: "/",
+  afterSignUpUrl: "/",
+  publishableKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+};
