fb21ae4a5c22fcd2dcace1690995fef2b03dfe61
Munrhalls <antarcticdepths71@gmail.com>
2025-02-07 22:57:20 +0100

    middleware.ts change

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,19 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+// import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+// export default clerkMiddleware();
 
+// export const config = {
+//   matcher: [
+//     "/((?!_next/image|_next/static|favicon.ico).*)",
+//     // "/api/(.*)" // Uncomment if you have API routes to protect
+//   ],
+// };
 export const config = {
   matcher: [
-    "/((?!_next/image|_next/static|favicon.ico).*)",
-    // "/api/(.*)" // Uncomment if you have API routes to protect
+    "/(api|trpc)(.*)",
+    "/dashboard",
+    "/account",
+    // Add only protected routes
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
   ],
 };
