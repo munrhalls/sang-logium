b79f829214d2c7d7f0bc6ce40764856725f12975
Munrhalls <antarcticdepths71@gmail.com>
2025-04-17 15:57:29 +0200

    add showing ... products on search page

--- app/(store)/search/page.tsx
+++ app/(store)/search/page.tsx
@@ -11,7 +11,7 @@ export default async function Page({
 
   if (!query) {
     return (
-      <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
+      <div className="flex flex-col items-center justify-top min-h-screen bg-slate-200 p-4">
         <div className="bg-white p-8 rounded-lg shadow-md w-ful max-w-4xl">
           <h1 className="text-3xl font-bold mb-6 text-center">
             No products found
@@ -25,10 +25,11 @@ export default async function Page({
   }
 
   const products = await searchProductsByName(query);
+  const productsCount = products.length;
 
-  if (!products.length) {
+  if (!productsCount) {
     return (
-      <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
+      <div className="flex flex-col items-center justify-top min-h-screen bg-slate-200 p-4">
         <div className="bg-white p-8 rounded-lg shadow-md w-ful max-w-4xl">
           <h1 className="text-3xl font-bold mb-6 text-center">
             No products found for: {query}
@@ -42,11 +43,16 @@ export default async function Page({
   }
 
   return (
-    <div className="flex flex-col items-center justsify-top min-h-screen bg-gray-100 p-4">
+    <div className="flex flex-col items-center justsify-top min-h-screen bg-slate-200 p-4">
       <div className="bg-white rounded-lg w-full max-w-4xl">
-        <h1 className="text-3xl font-bold text-center mb-6">
-          Search results for {query}
+        <h1 className="text-3xl font-bold text-center my-6">
+          Search results for: <span className="font-light">{query}</span>
         </h1>
+        <p className="text-gray text-center mb-6 font-oswald">
+          {productsCount} product{productsCount > 1 ? "s" : ""} found{" "}
+        </p>
+      </div>
+      <div className="rounded-lg w-full max-w-4xl mt-4">
         <ProductsGrid products={products} />
       </div>
     </div>
