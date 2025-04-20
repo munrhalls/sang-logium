0bac6170f35e9ca2f271dd7437174f17ad1c14d8
Munrhalls <antarcticdepths71@gmail.com>
2025-04-20 14:55:21 +0200

    PAGI

--- app/components/ui/pagination/Pagination.tsx
+++ app/components/ui/pagination/Pagination.tsx
@@ -6,12 +6,22 @@ const totalPages = Math.ceil(pagesCount / pageSize);
 const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1);
 console.log("pageNumbers", pageNumbers);
 
+const initialNums = [1, 2, 3];
+const throughNums = [7];
+const lastNum = 12;
+
 const NumberButton = function (n) {
-  return <button>{n}</button>;
+  return <button className="mx-1">{n}</button>;
 };
 
 const PageNumbers = function () {
-  return <div></div>;
+  return (
+    <div className="flex">
+      {initialNums.map((pageNum) => NumberButton(pageNum))}...
+      {throughNums.map((pageNum) => NumberButton(pageNum))}...
+      {NumberButton(lastNum)}
+    </div>
+  );
 };
 
 const PrevButton = function () {
@@ -26,9 +36,11 @@ export default function Pagination() {
   return (
     <div>
       <h1>Page</h1>
-      <PrevButton />
-      <PageNumbers />
-      <NextButton />
+      <div className="flex justify-around items-center">
+        <PrevButton />
+        <PageNumbers />
+        <NextButton />
+      </div>
     </div>
   );
 }
