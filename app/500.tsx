037c354a61397749a7b30d4695740d80e784d0de
Munrhalls <antarcticdepths71@gmail.com>
2025-07-06 17:17:47 +0200

    deploy

---
+++ app/500.tsx
@@ -0,0 +1,9 @@
+export default function Custom500() {
+  return (
+    <html>
+      <body>
+        <h1>500 - Server-side error occurred</h1>
+      </body>
+    </html>
+  );
+}
---
+++ app/error.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function Error({ reset }: { reset: () => void }) {
+  return (
+    <html>
+      <body>
+        <h2>Something went wrong!</h2>
+        <button onClick={() => reset()}>Try again</button>
+      </body>
+    </html>
+  );
+}
