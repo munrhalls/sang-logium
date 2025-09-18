4af7a005c3565013899f32d1364bcca28353c1a9
Munrhalls <antarcticdepths71@gmail.com>
2025-09-18 16:23:58 +0200

    modif authenticated view

--- app/components/features/auth/Authentication.tsx
+++ app/components/features/auth/Authentication.tsx
@@ -3,15 +3,17 @@ import { SignedOut } from "@clerk/nextjs";
 import SignInBtn from "./SignInBtn";
 import AuthenticatedView from "./AuthenticatedView";
 
-export default function Authentication() {
+export default async function Authentication() {
   return (
     <div className="text-white">
-      <SignedIn>
+      <AuthenticatedView />
+
+      {/* <SignedIn>
         <AuthenticatedView />
       </SignedIn>
       <SignedOut>
         <SignInBtn />
-      </SignedOut>
+      </SignedOut> */}
     </div>
   );
 }
