83723baece7e2e9d2932dd39fe2774df4d1dc539
Munrhalls <antarcticdepths71@gmail.com>
2025-09-20 15:03:03 +0200

    hero img looks & performs

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -12,30 +12,28 @@ export const dynamic = "force-static";
 
 export default async function Page() {
   return (
-    <>
-      <main className="h-full relative">
-        <HeroCommercials />
-        <div className="grid grid-cols-[auto_8fr_auto] xl:grid-cols-[1fr_8fr_1fr] mx-auto">
-          <div className="mx-auto col-start-2 col-end-3 max-w-[1400px]">
-            <BrandsWall />
-            <Bestsellers />
-          </div>
-          <div className="col-start-1 col-end-4">
-            <NewestRelease />
-          </div>
-          <div className="mx-auto col-start-2 col-end-3 max-w-[1400px]">
-            <ExtremeQuality />
-            <FeaturedProducts />
-          </div>
-          <div className="col-start-1 col-end-4">
-            <MonthProduct />
-          </div>
-          <div className="col-start-1 col-end-4">
-            <MainCategories />
-          </div>
+    <main className="h-full relative overflow-x-hidden">
+      <HeroCommercials />
+      <div className="grid grid-cols-[auto_8fr_auto] xl:grid-cols-[1fr_8fr_1fr] mx-auto">
+        <div className="mx-auto col-start-2 col-end-3 max-w-[1400px]">
+          <BrandsWall />
+          <Bestsellers />
         </div>
-        <Footer />
-      </main>
-    </>
+        <div className="col-start-1 col-end-4">
+          <NewestRelease />
+        </div>
+        <div className="mx-auto col-start-2 col-end-3 max-w-[1400px]">
+          <ExtremeQuality />
+          <FeaturedProducts />
+        </div>
+        <div className="col-start-1 col-end-4">
+          <MonthProduct />
+        </div>
+        <div className="col-start-1 col-end-4">
+          <MainCategories />
+        </div>
+      </div>
+      <Footer />
+    </main>
   );
 }
