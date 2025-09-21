0d7326202a8d9650f9080ea5a9254d4408127b53
Munrhalls <antarcticdepths71@gmail.com>
2025-09-21 14:33:50 +0200

    categories loaded suspense

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -7,12 +7,12 @@ import FeaturedProducts from "../components/features/homepage/featured-products/
 import MainCategories from "../components/features/homepage/main-categories/MainCategories";
 import HeroCommercials from "../components/features/homepage/hero-commercials/HeroCommercials";
 import Footer from "../components/layout/footer/Footer";
+import { Suspense } from "react";
 export const revalidate = 300;
 export const dynamic = "force-static";
 
 export default async function Page() {
   // TODO BATCH FETCH ALL COMMERCIALS THERE
-
 
   return (
     <main className="h-full relative overflow-x-hidden">
@@ -20,7 +20,9 @@ export default async function Page() {
       <div className="grid grid-cols-[auto_8fr_auto] xl:grid-cols-[1fr_8fr_1fr] mx-auto">
         <div className="mx-auto col-start-2 col-end-3 max-w-[1400px]">
           <BrandsWall />
-          <Bestsellers />
+          <Suspense>
+            <Bestsellers />
+          </Suspense>
         </div>
         <div className="col-start-1 col-end-4">
           <NewestRelease />
