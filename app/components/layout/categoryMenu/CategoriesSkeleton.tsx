cae15902d17517176f10131c2d14323c5abba9fa
Munrhalls <antarcticdepths71@gmail.com>
2025-09-22 14:40:49 +0200

    categories loader

--- app/components/layout/categoryMenu/CategoriesSkeleton.tsx
+++ app/components/layout/categoryMenu/CategoriesSkeleton.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function CategoriesSkeleton() {
-  return <div className="bg-red-700 text-white">Loading categories...</div>;
+  return (
+    <div className="bg-black text-white flex items-center justify-center font-bold tracking-wide">
+      Loading...
+    </div>
+  );
 }
