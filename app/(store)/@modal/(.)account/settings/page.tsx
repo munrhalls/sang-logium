6aaa2ab23ac67c9e8ed147816be4f7fd50890ca1
Munrhalls <antarcticdepths71@gmail.com>
2025-10-11 18:00:01 +0200

    account xp

--- app/(store)/@modal/(.)account/settings/page.tsx
+++
@@ -1,6 +0,0 @@
-import { UserButton } from "@clerk/nextjs";
-import { DotIcon } from "lucide-react";
-
-export default function Header() {
-  return <div className="flex justify-end p-4">asdas</div>;
-}
