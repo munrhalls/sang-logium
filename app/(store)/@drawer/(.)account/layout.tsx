66b381f5c080370a49f1e08a1822153083123948
Munrhalls <antarcticdepths71@gmail.com>
2025-10-11 19:31:41 +0200

    account & tracking routing xp

--- app/(store)/@drawer/(.)account/layout.tsx
+++ app/(store)/@drawer/(.)account/layout.tsx
@@ -22,7 +22,7 @@ export default function AccountLayout({
     <>
       <div
         onClick={handleExit}
-        className="fixed inset-0 top-[4rem] z-40 sm:top-[6rem] md:bg-black/50"
+        className="fixed inset-0 bottom-14 top-[4rem] z-40 sm:bottom-0 sm:top-[6rem] md:bg-black/50"
       />
 
       <div className="fixed bottom-14 right-0 top-[4rem] z-50 w-full overflow-y-scroll bg-white shadow-lg sm:bottom-0 sm:top-[6rem] sm:w-11/12 sm:max-w-3xl xl:max-w-6xl">
