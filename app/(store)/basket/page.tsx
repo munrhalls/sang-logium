3c600f4fec0821997679576f69ddce31e59db3b9
Munrhalls <antarcticdepths71@gmail.com>
2025-10-16 14:09:09 +0200

    basket refactor

--- app/(store)/basket/page.tsx
+++ app/(store)/basket/page.tsx
@@ -5,6 +5,7 @@ import SegmentTitle from "@/app/components/ui/segment-title/SegmentTitle";
 import { useBasketStore } from "@/store/store";
 import BasketControls from "@/app/components/features/basket/BasketControls";
 import Empty from "./Empty";
+import Basket from "./Basket";
 
 export default function BasketPage() {
   const basket = useBasketStore((s) => s.basket);
@@ -31,44 +32,7 @@ export default function BasketPage() {
               <div className="text-center">Quantity</div>
               <div></div>
             </div>
-            {basket.map((item) => (
-              <div
-                key={item._id + "Basket page"}
-                className="grid grid-cols-1 items-center gap-5 border-b border-gray-200 p-5 transition-colors hover:bg-gray-50 lg:grid-cols-[3fr_1fr_1fr_auto]"
-              >
-                <div className="flex items-center gap-5">
-                  <div className="flex h-24 w-24 items-center justify-center overflow-hidden rounded-sm bg-gray-100">
-                    <ShoppingCartIcon className="h-10 w-10 text-gray-400" />
-                  </div>
-                  <div>
-                    <Link href={`/product/${item._id}`}>
-                      <h3 className="text-lg font-medium text-gray-900 hover:underline">
-                        {item.name}
-                      </h3>
-                    </Link>
-                    <p className="mt-2 text-sm text-gray-500 lg:hidden">
-                      <span className="font-medium">
-                        ${item.price.toFixed(2)}
-                      </span>{" "}
-                      × {item.quantity}
-                    </p>
-                  </div>
-                </div>
-                <div className="hidden items-center justify-center lg:flex">
-                  <div className="font-medium text-gray-900">
-                    ${item.price.toFixed(2)}
-                  </div>
-                </div>
-                <div className="flex items-center lg:justify-center">
-                  <div className="mr-3 text-sm font-medium text-gray-600 lg:hidden">
-                    Quantity:
-                  </div>
-                  <div className="flex items-center">
-                    <BasketControls product={item} />
-                  </div>
-                </div>
-              </div>
-            ))}
+            <Basket />
           </div>
           <div className="mt-6 lg:hidden">
             <Link
@@ -82,6 +46,7 @@ export default function BasketPage() {
         </div>
         <div className="lg:col-span-1">
           <div className="sticky top-4 rounded-sm bg-white p-6 shadow-sm">
+            {/* // order summary */}
             <h2 className="mb-6 border-b border-gray-200 pb-4 text-lg font-bold">
               Order Summary
             </h2>
