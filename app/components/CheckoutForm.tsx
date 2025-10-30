109f7c96851d6aa724c15825a3837fdde2397eed
Munrhalls <antarcticdepths71@gmail.com>
2025-10-30 16:52:00 +0100

    PAYMENTS STRIPE - checkout refactor

--- app/components/CheckoutForm.tsx
+++ app/components/CheckoutForm.tsx
@@ -1,43 +1,43 @@
-"use client";
+// "use client";
 
-import { useEffect, useState } from "react";
-import {
-  EmbeddedCheckoutProvider,
-  EmbeddedCheckout,
-} from "@stripe/react-stripe-js";
-import { stripePromise } from "@/lib/stripe-client";
+// import { useEffect, useState } from "react";
+// import {
+//   EmbeddedCheckoutProvider,
+//   EmbeddedCheckout,
+// } from "@stripe/react-stripe-js";
+// import { stripePromise } from "@/lib/stripe-client";
 
-interface CheckoutFormProps {
-  clientSecret: string;
-}
+// interface CheckoutFormProps {
+//   clientSecret: string;
+// }
 
-export default function CheckoutForm({ clientSecret }: CheckoutFormProps) {
-  const [isLoading, setIsLoading] = useState(true);
+// export default function CheckoutForm({ clientSecret }: CheckoutFormProps) {
+//   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    if (clientSecret) {
-      setIsLoading(false);
-    }
-  }, [clientSecret]);
+//   useEffect(() => {
+//     if (clientSecret) {
+//       setIsLoading(false);
+//     }
+//   }, [clientSecret]);
 
-  if (isLoading) {
-    return (
-      <div>
-        <p>Loading checkout...</p>
-      </div>
-    );
-  }
+//   if (isLoading) {
+//     return (
+//       <div>
+//         <p>Loading checkout...</p>
+//       </div>
+//     );
+//   }
 
-  return (
-    <div id="checkout">
-      <EmbeddedCheckoutProvider
-        stripe={stripePromise}
-        options={{
-          clientSecret,
-        }}
-      >
-        <EmbeddedCheckout />
-      </EmbeddedCheckoutProvider>
-    </div>
-  );
-}
+//   return (
+//     <div id="checkout">
+//       <EmbeddedCheckoutProvider
+//         stripe={stripePromise}
+//         options={{
+//           clientSecret,
+//         }}
+//       >
+//         <EmbeddedCheckout />
+//       </EmbeddedCheckoutProvider>
+//     </div>
+//   );
+// }
