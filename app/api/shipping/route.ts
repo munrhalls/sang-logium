83dd2a2a8023f8030babd3a3cdea1efcbb5f265a
Munrhalls <antarcticdepths71@gmail.com>
2025-11-03 08:06:04 +0100

    SHIPPING ADDRESS API - DEVELOPING IN ISOLATION

--- app/api/shipping/route.ts
+++ app/api/shipping/route.ts
@@ -1,19 +1,49 @@
-export async function POST(req: Request) {
-  console.log(req, "req");
-  // const body = await req.json();
-  // console.log(body, " --- REQUEST BODY IN SHIPPING ROUTE");
-  // handle input
-  // call external API (google maps address validation api)
-  // handle output
+interface AddressLines {
+  regionCode: string;
+  locality: string;
+  addressLines: [string];
+}
 
-  //
-  return new Response(
-    JSON.stringify({ message: "Address validation not yet implemented" }),
-    {
-      status: 200,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
+interface GoogleValidationAPIRequest {
+  address: AddressLines;
+}
+
+export async function POST(req: Request) {
+  const body = await req.json();
+  const address = body;
+
+  // Validate address with external API (google maps address validation api)
+
+  const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
+  const validationURL = `https://addressvalidation.googleapis.com/v1:validateAddress?key=${apiKey}`;
+
+  const addressLines = [address.postalCode, address.street, address.city].join(
+    " "
   );
+
+  const validationRequestBody: GoogleValidationAPIRequest = {
+    address: {
+      regionCode: address.country,
+      locality: address.city,
+      addressLines: [addressLines],
+    },
+  };
+  console.log(validationRequestBody);
+
+  const validationResponse = await fetch(validationURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(validationRequestBody),
+  });
+
+  console.log("Address validation response:", validationResponse);
+
+  return new Response(JSON.stringify(validationResponse), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
