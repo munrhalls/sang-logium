d82a840b568f7dc0ff734f4614e84ede786ded77
Munrhalls <antarcticdepths71@gmail.com>
2025-11-06 14:15:23 +0100

    Checkout - make confirmation page display shippingAddress nicely

--- app/(store)/checkout/shipping/confirmation/page.tsx
+++ app/(store)/checkout/shipping/confirmation/page.tsx
@@ -5,9 +5,6 @@ import { redirect } from "next/navigation";
 
 export default function Page() {
   const { shippingAPIValidation, shippingAddress } = useCheckout();
-  // IF the value === 'CONFIRMED', just show payment button / link to payment page, still show small update button though, which takes the user back to shipping page but with prefilled data
-  // IF the value === 'PARTIAL' - everything is the same except warning message is shown that the address was only partially confirmed by the maps - gentle notice to user if they're sure to proceed and aware that responsibility for wrong address is on them
-  // commit
 
   if (!shippingAPIValidation?.status) {
     redirect("/checkout/shipping");
