bd2a5aa55786e02e46399eb56edc8dea205ac135
Munrhalls <antarcticdepths71@gmail.com>
2025-11-06 19:32:15 +0100

    Checkout layout- refactor/debug typescript - context typed as nullable but never actually null

--- app/(store)/checkout/shipping/page.tsx
+++ app/(store)/checkout/shipping/page.tsx
@@ -5,7 +5,6 @@ import Loader from "@/app/components/common/Loader";
 import { useRouter } from "next/navigation";
 import { Link } from "lucide-react";
 import { useCheckout } from "@/app/(store)/checkout/layout";
-import CheckoutContextType from "@/app/(store)/checkout/layout";
 
 type FormData = {
   regionCode: string;
