40300865168511ca3a5d88b6764c02d0ec4cefc3
Munrhalls <antarcticdepths71@gmail.com>
2025-11-06 11:32:10 +0100

    CHECKOUT - delete confirmation shipping modal, make it page.

--- app/(store)/checkout/shipping/confirmation/page.tsx
+++ app/(store)/checkout/shipping/confirmation/page.tsx
@@ -1,10 +1,18 @@
 import Link from "next/link";
-import ShippingConfirmation from "@/app/components/features/checkout/ShippingConfirmation";
+import { useCheckout } from "@/app/(store)/checkout/layout";
 
-export default function page() {
+export default function Page() {
+  const { shippingAPIValidation } = useCheckout();
   return (
     <div>
-      <ShippingConfirmation />
+      <div className="bg-slate-600">
+        <h2 className="text-lg font-bold">
+          Shipping Address CONFIRMATION STATUS
+        </h2>
+        <p className="text-white">
+          Status: {shippingAPIValidation?.status || "Loading..."}
+        </p>
+      </div>
       <Link href="/checkout/payment">Payment</Link>
     </div>
   );
