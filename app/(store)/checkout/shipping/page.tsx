a0c5f518053b2e46a59b9d77b692ac45f1feb72b
Munrhalls <antarcticdepths71@gmail.com>
2025-11-11 18:53:59 +0100

    Checkout / shipping - refactor

--- app/(store)/checkout/shipping/page.tsx
+++ app/(store)/checkout/shipping/page.tsx
@@ -1,11 +1,10 @@
 "use client";
-import { useForm } from "react-hook-form";
-import Loader from "@/app/components/common/Loader";
 import { useRouter } from "next/navigation";
 import { useCheckout } from "@/app/(store)/checkout/layout";
 import { useState } from "react";
 import { useSearchParams } from "next/navigation";
 import ConfirmationView from "./confirmation/ConfirmationView";
+import ShippingFormView from "./ShippingFormView";
 
 type FormData = {
   regionCode: string;
@@ -14,188 +13,6 @@ type FormData = {
   streetNumber: string;
   city: string;
 };
-
-const FormView = function ({
-  handleAddressSubmit,
-  isLoading,
-}: {
-  handleAddressSubmit: (data: FormData) => void;
-  isLoading: boolean;
-}) {
-  const { addressApiValidation, shippingAddress } = useCheckout();
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isValid },
-  } = useForm<FormData>({
-    mode: "onBlur",
-    defaultValues: shippingAddress ?? undefined,
-  });
-
-  return (
-    <div className="flex min-h-screen justify-center">
-      <div className="relative rounded bg-white p-4">
-        <div className="relative min-h-96 w-80">
-          {!isLoading && (
-            <form onSubmit={handleSubmit(handleAddressSubmit)}>
-              <h2 className="mb-4 text-lg font-bold">Enter Shipping Address</h2>
-              <p className="text-sm font-black tracking-wide">Country</p>
-              <select
-                {...register("regionCode", { required: true })}
-                className="mb-4 w-full border border-gray-300 p-2"
-              >
-                <option value="PL">Poland</option>
-                <option value="EN">England</option>
-              </select>
-              <p className="text-sm font-black tracking-wide">Postal code</p>
-              <input
-                {...register("postalCode", {
-                  required: "Postal Code is required.",
-                  minLength: {
-                    value: 5,
-                    message: "Postal Code must be at least 5 characters.",
-                  },
-                  maxLength: {
-                    value: 10,
-                    message: "Postal Code cannot exceed 10 characters.",
-                  },
-                  pattern: {
-                    value: /^[a-zA-Z0-9\s-]+$/,
-                    message:
-                      "Invalid format. Use letters, numbers, and spaces/hyphens.",
-                  },
-                })}
-                type="text"
-                placeholder="Postal Code"
-                className="mb-2 w-full border border-gray-300 p-2"
-              />
-              {errors.postalCode && (
-                <p className="mb-4 text-xs font-semibold text-red-500">
-                  {errors.postalCode.message || "Invalid Postal Code."}
-                </p>
-              )}
-
-              <div className="grid grid-cols-8 gap-2">
-                <div className="col-span-6">
-                  <p className="text-sm font-black tracking-wide">Street</p>
-                  <input
-                    {...register("street", {
-                      required: "Street is required.",
-                      minLength: {
-                        value: 3,
-                        message: "Street name must be at least 3 characters.",
-                      },
-                      maxLength: {
-                        value: 70,
-                        message: "Street name must be under 70 characters.",
-                      },
-                    })}
-                    type="text"
-                    placeholder="Street"
-                    className="mb-2 w-full border border-gray-300 p-2"
-                  />
-                  {errors.street && (
-                    <p className="mb-4 text-xs font-semibold text-red-500">
-                      {errors.street.message}
-                    </p>
-                  )}
-                </div>
-
-                <div className="col-span-2">
-                  <p className="text-sm font-black tracking-wide">Number</p>
-                  <input
-                    {...register("streetNumber", {
-                      required: "Number is required.",
-                      minLength: {
-                        value: 1,
-                        message: "Must be at least 1 character.",
-                      },
-                      maxLength: {
-                        value: 10,
-                        message: "Number cannot exceed 10 characters.",
-                      },
-                      pattern: {
-                        value: /^[a-zA-Z0-9\-\/]+$/,
-                        message:
-                          "Invalid format. Use numbers, letters, -, or /.",
-                      },
-                    })}
-                    type="text"
-                    placeholder="..."
-                    className="mb-2 flex w-full items-center justify-center border border-gray-300 p-2"
-                  />
-                  {errors.streetNumber && (
-                    <p className="mb-4 text-xs font-semibold text-red-500">
-                      {errors.streetNumber.message}
-                    </p>
-                  )}
-                </div>
-              </div>
-              <p className="text-sm font-black tracking-wide">City</p>
-              <input
-                {...register("city", {
-                  required: "City name is required.",
-                  minLength: {
-                    value: 3,
-                    message: "City name must be at least 3 characters long.",
-                  },
-                  maxLength: {
-                    value: 50,
-                    message: "City name cannot exceed 50 characters.",
-                  },
-                  pattern: {
-                    value: /^[a-zA-Z\s-]+$/,
-                    message:
-                      "Invalid format. City name can only contain letters, spaces, or hyphens.",
-                  },
-                })}
-                type="text"
-                placeholder="City"
-                className="mb-2 w-full border border-gray-300 p-2"
-              />
-              {errors.city && (
-                <p className="mb-4 text-xs font-semibold text-red-500">
-                  {errors.city.message}
-                </p>
-              )}
-              <button
-                disabled={!isValid}
-                type="submit"
-                className={`w-full rounded px-4 py-2 ${
-                  !isValid
-                    ? "cursor-not-allowed bg-gray-400 text-gray-600"
-                    : "bg-black text-white"
-                }`}
-              >
-                Submit Address
-              </button>
-            </form>
-          )}
-          {addressApiValidation === "FIX" && (
-            <p className="mt-4 text-sm text-red-600">
-              Could not locate provided address on the map. Please review and
-              make sure it is correct.
-            </p>
-          )}
-          {addressApiValidation === "ERROR" && (
-            <p className="mt-4 text-sm text-red-600">
-              An error occurred while validating your address. Please try again.
-            </p>
-          )}
-          {isLoading && (
-            <Loader message="Processing..." color="border-t-black" />
-          )}
-        </div>
-      </div>
-    </div>
-  );
-};
-// TODO FIX FLICKER UPON REDIRECT TO //checkout/shipping/confirmation
-
-// TODO handle API error state in the UI
-
-// TODO prevent DDOS etc. by disabling submit button while loading and by limiting amount submits per minute / hour / day
 
 export default function Page() {
   const { validateShipping } = useCheckout();
@@ -218,6 +35,9 @@ export default function Page() {
   return step === "confirmation" ? (
     <ConfirmationView />
   ) : (
-    <FormView handleAddressSubmit={handleAddressSubmit} isLoading={isLoading} />
+    <ShippingFormView
+      handleAddressSubmit={handleAddressSubmit}
+      isLoading={isLoading}
+    />
   );
 }
