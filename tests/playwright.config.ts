839be907cd6febe983c8b3f3ebe844b52c006dd2
Munrhalls <antarcticdepths71@gmail.com>
2025-11-20 17:38:23 +0100

    delete old deps

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
-  testDir: "./tests",
+  testDir: "./",
+  testIgnore: "**/jest/**",
+
+  webServer: {
+    command: "npm run dev:test",
+    url: "http://localhost:3001",
+    reuseExistingServer: !process.env.CI,
+  },
 
   use: {
-    baseURL: "http://localhost:3000",
+    baseURL: "http://localhost:3001",
   },
 
   projects: [
