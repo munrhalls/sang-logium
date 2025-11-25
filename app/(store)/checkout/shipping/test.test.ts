29e3f47eeee99cfe8a1a72e4a5efd7179fef9156
Munrhalls <antarcticdepths71@gmail.com>
2025-11-25 08:57:57 +0100

    Storybook

--- app/(store)/checkout/shipping/test.test.ts
+++ app/(store)/checkout/shipping/test.test.ts
@@ -1,12 +1,11 @@
 import Address from "app/(store)/checkout/checkout.types";
-
 import { test } from "vitest";
-import { sum } from "./test";
+import { test } from "./testf";
 
 test("sum function", () => {
   const a = 2;
   const b = 14;
-  const result = sum({ a: 1, b: 2 }, 10);
+  const result = test({ a: 1, b: 2 }, 10);
 
   console.log({ a, b, result });
 });
--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -4,14 +4,10 @@ export default defineConfig({
   testDir: "./",
   testIgnore: "**/jest/**",
 
-  webServer: {
-    command: "npm run dev:test",
-    url: "http://localhost:3001",
-    reuseExistingServer: !process.env.CI,
-  },
-
   use: {
-    baseURL: "http://localhost:3001",
+    baseURL: "https://localhost:3000",
+    trace: "on",
+    // ignoreHTTPSErrors: true,
   },
 
   projects: [
