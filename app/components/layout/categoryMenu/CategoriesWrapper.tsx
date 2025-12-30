130c369248fd3d6f9df089a36aa280c902c28876
Munrhalls <antarcticdepths71@gmail.com>
2025-12-30 12:31:16 +0100

    Refactor categories - get the raw categories data out into json

--- app/components/layout/categoryMenu/CategoriesWrapper.tsx
+++ app/components/layout/categoryMenu/CategoriesWrapper.tsx
@@ -2,6 +2,7 @@ import { getAllCategories } from "@/sanity/lib/products/getAllCategories";
 import CategoriesNav from "./CategoriesNav";
 export default async function CategoriesWrapper() {
   const categories = await getAllCategories();
+  console.dir(categories, { depth: null });
   categories.sort((a, b) => {
     if (a?.order === undefined || b?.order === undefined) return 0;
     return a?.order - b?.order;
