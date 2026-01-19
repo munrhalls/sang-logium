ab86c41ae88e3b54d10294e34adc9cf7bebbfc2b
Munrhalls <antarcticdepths71@gmail.com>
2026-01-19 11:00:30 +0100

    Delete irrelevant sanity/studio

--- app/(studio)/studio/[[...tool]]/page.tsx
+++ app/(studio)/studio/[[...tool]]/page.tsx
@@ -1,12 +1,7 @@
 "use client";
 import { NextStudio } from "next-sanity/studio";
 import config from "../../../../sanity.config";
-import StyledComponentsRegistry from "./../../lib/StyledComponentsRegistry";
 
 export default function StudioPage() {
-  return (
-    <StyledComponentsRegistry>
-      <NextStudio config={config} />
-    </StyledComponentsRegistry>
-  );
+  return <NextStudio config={config} />;
 }
